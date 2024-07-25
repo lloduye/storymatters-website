6d5fd6193f7f6692ea6c88640faf4e51a89a929e
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2024-07-25 16:12:29 -0300

    Updated today

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import './Navbar.css'
+
+const Navbar = (props) => {
+  return (
+    <header className="header">
+      <a href="/" className="logo">Logo</a>
+      <nav className="navbar">
+        <a href="">What we do</a>
+        <a href="">Where we work</a>
+        <a href="">About us</a>
+        <a href="">News & Updates</a>
+        <a href="">Donate</a>
+      </nav>
+      
+    </header>
+  )
+};
+
+export default Navbar;
